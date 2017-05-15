26e0a6ca0cd2f05cfbc8ba2982622840f93f9751
Toni Jovanoski <minimoog77@gmail.com>
2017-05-16 00:43:51 +0200

    Add Pixel struct

---
+++ pallete/Pallete.swift
@@ -0,0 +1,37 @@
+//
+//  Pallete.swift
+//  pallete
+//
+//  Created by Toni Jovanoski on 5/16/17.
+//  Copyright © 2017 Antonie Jovanoski. All rights reserved.
+//
+
+import UIKit
+
+public struct Pixel {
+    public var value: UInt32
+    
+    //red
+    public var R: UInt8 {
+        get { return UInt8(value & 0xFF); }
+        set { value = UInt32(newValue) | (value & 0xFFFFFF00) }
+    }
+    
+    //green
+    public var G: UInt8 {
+        get { return UInt8((value >> 8) & 0xFF) }
+        set { value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF) }
+    }
+    
+    //blue
+    public var B: UInt8 {
+        get { return UInt8((value >> 16) & 0xFF) }
+        set { value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF) }
+    }
+    
+    //alpha
+    public var A: UInt8 {
+        get { return UInt8((value >> 24) & 0xFF) }
+        set { value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF) }
+    }
+}
