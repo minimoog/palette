e3dbb3dc338fc1316aa6b0490a6c2c64f7395f77
Toni Jovanoski <minimoog77@gmail.com>
2017-05-18 00:51:48 +0200

    Add function for calculating two points distance Add fumcton for calculating center of points array

--- pallete/RGBAImage.swift
+++ pallete/RGBAImage.swift
@@ -18,8 +18,11 @@ public struct RGBAImage {
             return nil
         }
         
+        width = Int(image.size.width)
+        height = Int(image.size.height)
+        
         let bytesPerRow = width * 4
-        let imageData = UnsafeMutableBufferPointer<Pixel>.allocate(capacity: width * height)
+        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         
         var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
@@ -36,7 +39,7 @@ public struct RGBAImage {
             return nil
         }
         
-        imageContext.draw(cgImage, CGRect(origin: .zero, size: size))
+        imageContext.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
         
         pixels = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
     }
@@ -47,7 +50,35 @@ public struct RGBAImage {
         }
         
         let address = y * width + x
-        return pixels?[address]
+        return pixels[address]
+    }
+    
+    fileprivate func euclidean(p1: Pixel, p2: Pixel) -> Float {
+        let rd: Float = Float(p1.R) - Float(p2.R)
+        let gd: Float = Float(p1.G) - Float(p2.G)
+        let bd: Float = Float(p1.B) - Float(p2.B)
+        
+        let s: Float = rd + gd + bd
+        
+        return sqrtf(s)
+    }
+    
+    fileprivate func calculateCentre(points: [Pixel]) -> Pixel {
+        let ra: Float = 0.0
+        let ba: Float = 0.0
+        let ga: Float = 0.0
+        
+        let sumra: Float = points.reduce(ra) { $0 + Float($1.R) }
+        let sumga: Float = points.reduce(ga) { $0 + Float($1.G) }
+        let sumba: Float = points.reduce(ba) { $0 + Float($1.B) }
+        
+        var pixel: Pixel = Pixel(value: 0)
+        
+        pixel.R = UInt8(sumra / Float(points.count))
+        pixel.G = UInt8(sumga / Float(points.count))
+        pixel.B = UInt8(sumba / Float(points.count))
+        
+        return pixel
     }
 }
 
